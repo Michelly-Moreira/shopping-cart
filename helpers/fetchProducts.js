90aae415d443d6d2e3fe2ca48dfbb193ebad09ab
Michelly <michelly.daiana@hotmail.com>
2022-10-12 15:22:27 -0300

    requirement 2

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,7 +1,21 @@
-const fetchProducts = () => {
-  // seu código aqui
+const fetchProducts = async () => {
+  try {
+    const response = await fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador');
+    const responseValue = await response.json();
+    // console.log(responseValue);
+    return responseValue;
+  } catch (error) {
+    console.log('Error', error);
+  }
 };
+ // fetchProducts(); 
 
+/* const getProduct = async () => {
+  await fetchProducts();
+  return fetchProducts;
+};
+getProduct(); */
+ 
 if (typeof module !== 'undefined') {
   module.exports = {
     fetchProducts,
--- script.js
+++ script.js
@@ -1,19 +1,27 @@
 // Esse tipo de comentário que estão antes de todas as funções são chamados de JSdoc,
 // experimente passar o mouse sobre o nome das funções e verá que elas possuem descrições! 
 
-// Fique a vontade para modificar o código já escrito e criar suas próprias funções!
+// const { fetchProducts } = require("./helpers/fetchProducts");
+
+const resultProducts = async () => {
+const getProducts = await fetchProducts();
+const { results } = getProducts;
+// console.log(results);
+return results;
+  };
+  resultProducts();
 
 /**
  * Função responsável por criar e retornar o elemento de imagem do produto.
  * @param {string} imageSource - URL da imagem.
  * @returns {Element} Elemento de imagem do produto.
- */
-const createProductImageElement = (imageSource) => {
+ */ 
+  const createProductImageElement = (imageSource) => {
   const img = document.createElement('img');
   img.className = 'item__image';
   img.src = imageSource;
   return img;
-};
+}; 
 
 /**
  * Função responsável por criar e retornar qualquer elemento.
@@ -22,7 +30,7 @@ const createProductImageElement = (imageSource) => {
  * @param {string} innerText - Texto do elemento.
  * @returns {Element} Elemento criado.
  */
-const createCustomElement = (element, className, innerText) => {
+  const createCustomElement = (element, className, innerText) => {
   const e = document.createElement(element);
   e.className = className;
   e.innerText = innerText;
@@ -37,24 +45,35 @@ const createCustomElement = (element, className, innerText) => {
  * @param {string} product.thumbnail - URL da imagem do produto.
  * @returns {Element} Elemento de produto.
  */
-const createProductItemElement = ({ id, title, thumbnail }) => {
+
+  const createProductItemElement = ({ id, title, thumbnail }) => {
   const section = document.createElement('section');
   section.className = 'item';
-
   section.appendChild(createCustomElement('span', 'item_id', id));
   section.appendChild(createCustomElement('span', 'item__title', title));
   section.appendChild(createProductImageElement(thumbnail));
   section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
-
+  
   return section;
-};
+}; 
 
+const createItem = async () => {
+  const getElement = document.querySelector('.items');
+  const inputElement = await resultProducts();
+  // console.log(inputElement);
+  inputElement.forEach((element) => {
+    const inputProduct = createProductItemElement(element);
+     console.log(inputProduct);
+     getElement.appendChild(createProductItemElement(element));
+  });
+};
+createItem();
 /**
  * Função que recupera o ID do produto passado como parâmetro.
  * @param {Element} product - Elemento do produto.
  * @returns {string} ID do produto.
  */
-const getIdFromProductItem = (product) => product.querySelector('span.id').innerText;
+ const getIdFromProductItem = (product) => product.querySelector('span.id').innerText;
 
 /**
  * Função responsável por criar e retornar um item do carrinho.
@@ -64,7 +83,7 @@ const getIdFromProductItem = (product) => product.querySelector('span.id').inner
  * @param {string} product.price - Preço do produto.
  * @returns {Element} Elemento de um item do carrinho.
  */
-const createCartItemElement = ({ id, title, price }) => {
+  const createCartItemElement = ({ id, title, price }) => {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `ID: ${id} | TITLE: ${title} | PRICE: $${price}`;
@@ -72,4 +91,5 @@ const createCartItemElement = ({ id, title, price }) => {
   return li;
 };
 
-window.onload = () => { };
+window.onload = () => {
+ };
